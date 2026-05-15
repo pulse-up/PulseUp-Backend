ca77bfc43981b2975efa8b0c497e9b18f6eee7ee
Siphokuhle Millior Angela Nyana <218123191@mycput.ac.za>
2026-05-15 02:29:10 +0200

    :construction:

--- PulseUp2026/src/main/java/ac/za/cput/pulseup2026/repository/AppointmentRepository.java
+++ PulseUp2026/src/main/java/ac/za/cput/pulseup2026/repository/AppointmentRepository.java
@@ -1,6 +1,9 @@
 package ac.za.cput.pulseup2026.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ac.za.cput.pulseup2026.domain.Appointment;
 
+@Repository
 public interface AppointmentRepository extends IRepository<Appointment, Long>{
 }
